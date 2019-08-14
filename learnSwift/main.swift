cefbdc600ed86b8c5a87825226129783bac33cb7
Isaac Atif <a126827@a05041369.local>
2019-08-14 09:47:47 -0700

    Initial Commit

---
+++ learnSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  learnSwift
+//
+//  Created by Isaac Atif on 8/14/19.
+//  Copyright © 2019 Isaac Atif. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
