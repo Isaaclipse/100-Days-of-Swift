cbdf0e865fedbc480c8f1df440064c8e7db0bfd4
Isaac Atif <a126827@a05041369.federated.fds>
2019-08-15 16:06:43 -0700

    first commit

---
+++ learnSwift/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  learnSwift
+//
+//  Created by Isaac Atif on 8/15/19.
+//  Copyright © 2019 Isaac Atif. All rights reserved.
+//
+
+import Foundation
+
+
+func
--- learnSwift/main.swift
+++ learnSwift/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+func run () {
+    
+    let arguments = CommandLine.arguments
+    
+    if arguments.count > 1 {
+        
+        let greeting = arguments[1]
+        
+        print(greeting)
+        
+    }
+    
+    if arguments.count > 2 {
+        
+        let number = arguments[2]
+        
+        print(number)
+        
+    }
+    
+    print("Hello, World! This is my command line tool")
+    
+}
 
+run()
